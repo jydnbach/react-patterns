d78ae30a70d047a5f14cdc707dd9d4d4b162f1aa
jydnbach <jaydenkim95@gmail.com>
2024-06-30 11:36:16 +0900

    accordion jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from './components/Accordion/Accordion';
+import AccordionItem from './components/Accordion/AccordionItem';
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2></h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>We are in the business of ....</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We're working with local guides"
+          >
+            <article>
+              <p>We are not doing this along from out office.</p>
+              <p>Instead, we are working with local guides</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
