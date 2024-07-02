6effceebba00ac4ec69acd7d467c5d23f02a40f5
jydnbach <jaydenkim95@gmail.com>
2024-07-02 12:29:26 +0900

    refactor: added extra components: title & content

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,21 +1,3 @@
-import { useAccordionContext } from './Accordion';
-
-export default function AccordionItem({ id, title, children, className }) {
-  const { openItemId, toggleItem } = useAccordionContext();
-
-  const isOpen = openItemId === id;
-
-  return (
-    <li className={className}>
-      <h3 onClick={() => toggleItem(id)}>{title}</h3>
-
-      <div
-        className={
-          isOpen ? 'accordion-item-content open' : 'accordion-item-content'
-        }
-      >
-        {children}
-      </div>
-    </li>
-  );
+export default function AccordionItem({ children, className }) {
+  return <li className={className}>{children}</li>;
 }
